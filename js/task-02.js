8d23b5941b091d178ab8e305914b1929d628db5b
MariiaDovhan1 <125833245+MariiaDovhan1@users.noreply.github.com>
2023-07-18 13:00:54 +0200

    ghjkl

--- js/task-02.js
+++ js/task-02.js
@@ -7,12 +7,32 @@ const ingredients = [
   'Condiments',
 ];
 
-const ulEl = document.createElement("li");
-ulEl.textContent = ingredients;
-console.log(ulEl)
- const list = ingredients.reduce((str, item) => str + `<li>${item}</li>`, '');
 
-ulEl.innerHTML = list;
+
+
+const list = document.querySelector('#ingredients')
+const liArray = []
+
+ingredients.forEach(ingredient => {
+  const item = document.createElement('li')
+  item.className = 'item'
+  item.textContent = ingredient
+  liArray.push(item)
+})
+
+list.append(...liArray)
+console.log(liArray);
+
+
+
+
+
+// const ulEl = document.createElement("li");
+// ulEl.textContent = ingredients;
+// console.log(ulEl)
+//  const list = ingredients.reduce((str, item) => str + `<li>${item}</li>`, '');
+
+// ulEl.innerHTML = list;
 
 // Напиши скрипт, який для кожного елемента масиву ingredients:
 
